7aaf78e3e5bf4de69c0e5739089f06e66c54effb
PaoloBardi <76535976+PaoloBardi@users.noreply.github.com>
2020-12-27 15:45:56 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -4,6 +4,7 @@ with open("./data.csv", newline="") as fileDati:
     csvHandler = csv.reader(fileDati,delimiter=",")
     docenteInserito = input("Inserisci il docente [Name Surname]: ")
     if docenteInserito=="":
+            next(csvHandler)
             dati = [(riga[1],riga[3],riga[0]) for riga in csvHandler]
     else:
             dati = [(riga[1],riga[3],riga[0]) for riga in csvHandler if docenteInserito in riga[4]]       
